648e40302904220554ef500c13803ee578c6c142
prakuwebs <prakumargundala@gmail.com>
2026-02-24 23:12:18 +0530

    Update page.js

--- app/watch/page.js
+++ app/watch/page.js
@@ -1,17 +1,9 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-export default function Page() {
-  return (
-    <Suspense fallback={<div style={{color:"#fff", textAlign:"center", marginTop:"50px"}}>Loading player...</div>}>
-      <Watch />
-    </Suspense>
-  );
-}
-
-"use client";
-import { useSearchParams } from "next/navigation";
-
-function Watch() {
+function WatchPlayer() {
   const p = useSearchParams();
   const chat = p.get("chat");
   const msg = p.get("msg");
@@ -19,13 +11,21 @@ function Watch() {
   const src = `https://teleplays-mtproto-backend.onrender.com/stream/${chat}/${msg}`;
 
   return (
-    <div style={{display:"flex",justifyContent:"center",marginTop:"50px"}}>
-      <video 
-        src={src} 
-        controls 
-        autoPlay 
+    <div style={{ display:"flex", justifyContent:"center", marginTop:"50px" }}>
+      <video
+        src={src}
+        controls
+        autoPlay
         style={{ width:"90%", maxWidth:"900px", borderRadius:"8px" }}
       />
     </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div style={{color:"#fff", textAlign:"center", marginTop:"50px"}}>Loading player...</div>}>
+      <WatchPlayer />
+    </Suspense>
   );
 }
